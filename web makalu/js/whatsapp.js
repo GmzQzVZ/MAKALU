e16c9f4c8c2b8b4469e8e7d5a365d5d4131e06c4
GmzQzvZ <gmzqzvz@gmail.com>
2024-11-11 15:01:12 -0500

    visual

--- web makalu/js/whatsapp.js
+++ web makalu/js/whatsapp.js
@@ -4,7 +4,7 @@ var input_blanter = document.getElementById('wa_email');
 /* Whatsapp Settings */
 var walink = 'https://web.whatsapp.com/send',
     phone = '573229520608',
-    walink2 = 'hola, mucho gusto ni nombre es:',
+    walink2 = 'hola, mucho gusto mi nombre es:',
     text_yes = 'Enviado.',
     text_no = 'Complete todos los formularios y luego haga clic en Enviar.';
 
@@ -20,7 +20,8 @@ var input_select1 = $("#wa_select :selected").text(),
     input_name1 = $("#wa_name").val(),
     input_email1 = $("#wa_email").val(),
     input_number1 = $("#wa_number").val(),
-    input_url1 = $("#wa_url").val(),
+    input_adult1 = $("#wa_url").val(),
+    input_children1 = $("#wa_url").val(),
     input_textarea1 = $("#wa_textarea").val();
 
 /* Final Whatsapp URL */
@@ -29,8 +30,9 @@ var blanter_whatsapp = walink + '?phone=' + phone + '&text=' + walink2 + '%0A%0A
     '*Email Address* : ' + input_email1 + '%0A' +
     '*Select Option* : ' + input_select1 + '%0A' +
     '*Input Number* : ' + input_number1 + '%0A' +
-    '*URL/Link* : ' + input_url1 + '%0A' +
-    '*Description* : ' + input_textarea1 + '%0A';
+    '*Adultos* : ' + input_adult1 + '%0A' +
+    '*Niños* : ' + input_children1 + '%0A' +
+    '*Description de la Cotización* : ' + input_textarea1 + '%0A';
 
 /* Whatsapp Window Open */
 window.open(blanter_whatsapp,'_blank');
